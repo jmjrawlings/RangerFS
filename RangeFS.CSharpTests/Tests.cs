c2ab504bd4a47b75ee6a2d47d18bea7a27d89c26
jmjrawlings <hrkn@pm.me>
2019-03-17 23:15:14 +1000

    cs test

--- RangeFS.CSharpTests/Tests.cs
+++ RangeFS.CSharpTests/Tests.cs
@@ -5,15 +5,18 @@ using System.Linq;
 
 namespace RangeFS.CSharpTests
 {
+
     [TestClass]
     public class Tests
     {
         [TestMethod]
         public void TestMethod1()
         {
-            var a = (-10).ToRange(10);
-            var n = Range.Iterate<int, int>(-1, a).ToList();
-            Assert.IsTrue(n.Count == 1);
+            var a = (0).ToRange(10);
+            var b = (10).ToRange(20);
+            var c = a.Union(b);
+            var d = (0).ToRange(20);
+            Assert.AreEqual(c, d);
         }
     }
 }
