eb9827ebe8fdfde5651fbf773a5c2d5d96d51b69
jmjrawlings <hrkn@pm.me>
2019-03-17 16:24:03 +1000

    c# test project

---
+++ RangeFS.CSharpTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RangeFS.CSharpTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RangeFS.CSharpTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1be9b05-3f78-4a7b-87cc-094ccea559fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RangeFS.CSharpTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ranger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RangeFS.CSharpTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = (-10).ToRange(10);
+            var b = 0.ToRange().Buffer(10);
+            var (c, d) = b.Map(i => i * 10).Bisect(50);
+        }
+    }
+}
