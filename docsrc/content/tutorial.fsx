12302dcb4c5349911254340ecf52216e9ef167f7
Justin Rawlings <hrkn@pm.me>
2019-03-25 20:58:07 +1000

    docs

--- docsrc/content/tutorial.fsx
+++ docsrc/content/tutorial.fsx
@@ -31,31 +31,33 @@ printf "%O is equal to %O" a b
 
 (** Arguments to `Range.ofBounds` may be provided in any order *)
 (*** define-output:b ***)
-let c = Range.ofBounds 2 10
-let d = Range.ofBounds 10 2
-
-printf "%O" (c = d)
-(*** include-output:b ***)
+let c = Range.ofBounds 2 10 // {2..10}
+let d = Range.ofBounds 10 2 // {2..10}
 
 (** The special case where `range.Lo = range.Hi` is referred to as a `Point` and can be constructed by
 `Range.ofPoint` or it's operator equivalent `!` *)
 (*** define-output:c ***)
 // The following are all equivalent
-let e = Range.ofPoint 2.5
-let f = !2.5
-let g = Range.ofBounds 2.5 2.5
-let h = (2.5).ToRange()
-
-printf "%O = %O = %O = %O" e f g h
-(*** include-output:c ***)
+let e = Range.ofPoint 2.5 // {2.5}
+let f = !2.5 // {2.5}
+let g = Range.ofBounds 2.5 2.5 // {2.5}
+let h = (2.5).ToRange() // {2.5}
 
 (** We can also construct a `Range` from a sequence of points with `Range.ofSeq` *)
-let i = Range.ofSeq [ 7; 5; -2; -100; 50; 75]
-(*** include-value:h ***)
-
-let j = Range.ofSeq [ 'a'; 'b'; 'q'; 'z'; 'h'; 'j']
-
+let seq1 = Range.ofSeq [ 7; 5; -2; -100; 50; 75]
 (*** include-value:i ***)
+
+let seq2 = Range.ofSeq [ 'a'; 'b'; 'q'; 'z'; 'h'; 'j']
+(*** include-value:j ***)
+
+(** 
+Constructing a `Range` from an empty list results in the Empty Range. 
+Also available through `Range.empty`, the empty range is a natural way to represent the results
+of invalid operations such as the intersection of non-overlapping `Ranges`
+*)
+
+let seq3 : int Range = Range.ofSeq [] 
+let seq4 = Seq.empty |> Range.ofSeq |> Range.isEmpty // true
 
 (** `Ranges` of a certain magnitude may be constructed using `Range.ofSize` **)
 let k = 0.1 |> Range.ofSize 0.5 // {0.1 .. 0.6}
